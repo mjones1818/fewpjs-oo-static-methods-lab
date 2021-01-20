b68ff675c540475fdd5aa57d313940daa3cbba52
mjones1818 <michael.jones0532@gmail.com>
2021-01-20 17:54:05 -0600

    commit tree 2dec8e8aa88eb30b8f6a026908ff13d75a747d9e

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
+
+  static titleize(string) {
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'for', 'at', 'by', 'from', 'and']
+    let words = string.split(' ')
+    let firstWord = Formatter.capitalize(words[0])
+    let capitalized = words.slice(1).map(function(word) { 
+      return exceptions.includes(word) ? word : Formatter.capitalize(word)
+    })
+    
+    return firstWord + ' ' + capitalized.join(' ')
+  }
 }
+
